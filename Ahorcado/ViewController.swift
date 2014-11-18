24e6995f36f98ecc4c8e6d28e3ea64b265f2f586
Daniel Santiago Martinez <santiagodani@hotmail.com>
2014-11-18 13:53:39 +0100

    Initial Commit

---
+++ Ahorcado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ahorcado
+//
+//  Created by Daniel Santiago Martinez on 18/11/14.
+//  Copyright (c) 2014 Daniel Santiago Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
